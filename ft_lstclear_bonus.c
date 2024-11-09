b5de64dae2865baf5344a5bd68b0e0347ea1687d
yassn0 <yassinfradj28@gmail.com>
2024-11-09 17:41:10 +0100

    test2

---
+++ ft_lstclear_bonus.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear_bonus.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yfradj <yfradj@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/08 18:34:57 by yfradj            #+#    #+#             */
+/*   Updated: 2024/11/09 13:36:39 by yfradj           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*tmp;
+
+	tmp = NULL;
+	while (*lst)
+	{
+		tmp = (*lst)->next;
+		if ((*lst)->content)
+			ft_lstdelone(*lst, del);
+		*lst = tmp;
+	}
+	*lst = NULL;
+}
